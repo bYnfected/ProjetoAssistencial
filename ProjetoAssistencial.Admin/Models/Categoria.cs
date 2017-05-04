4ddbbe017f0ac2fe18f3a2d9ea5c1c98e65e9a07
Mauricio Calgaro <mauricio.calgaro@gmail.com>
2017-05-03 22:26:40 -0300

    ultima na aula

---
+++ ProjetoAssistencial.Admin/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAssistencial.Admin.Models
+{
+    public class Categoria
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ ProjetoAssistencial.Admin/Models/Entidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAssistencial.Admin.Models
+{
+    public class Entidade
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public bool Liberado { get; set; }
+
+        
+    }
+}
