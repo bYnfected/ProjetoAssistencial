1ddb85fa107c1ab008ac4b52268ac06c2ffb517e
Mauricio Calgaro <mauricio.calgaro@gmail.com>
2017-05-03 22:12:14 -0300

    d

---
+++ ProjetoAssistencial.Admin/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoAssistencial.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autenticar()
+        {
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
