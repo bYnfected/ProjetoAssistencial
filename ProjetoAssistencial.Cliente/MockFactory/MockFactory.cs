14240c9cdf4619e22ff712e4f2955e23cfe0680d
Mauricio Calgaro <mauricio.calgaro@gmail.com>
2017-05-10 16:22:20 -0300

    dd

---
+++ ProjetoAssistencial.Cliente/MockFactory/MockFactory.cs
@@ -0,0 +1,31 @@
+﻿using ProjetoAssistencial.Cliente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAssistencial.Cliente.MockFactory
+{
+    public class MockFactory
+    {
+        public static List<Acao> GerarListaCategorias(int qtd)
+        {
+            List<Acao> acoes = new List<Acao>();
+
+            for (int i = 0; i < qtd; i++)
+            {
+                acoes.Add(new Acao()
+                {
+                    Id = Guid.NewGuid(),
+                    IdEntidade = Guid.NewGuid(),
+                    IdCategoria = Guid.NewGuid(),
+                    Voluntarios = new List<Voluntario>,
+                    Descricao = "Descricao da Ação",
+                    DataHora = DateTime.Now
+            
+                });
+            }
+            return acoes;
+        }
+    }
+}
---
+++ ProjetoAssistencial.Cliente/Models/Acao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAssistencial.Cliente.Models
+{
+    public class Acao
+    {
+        public Guid Id { get; set; }
+
+        public Guid IdEntidade { get; set; }
+
+        public Guid IdCategoria { get; set; }
+        public List<Voluntario> Voluntarios { get; set; }
+        public string Descricao { get; set; }
+
+        public DateTime DataHora { get; set; }
+
+    }
+}
